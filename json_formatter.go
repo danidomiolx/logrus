8068024cbe795f997183102e869d3609cd838643
Daniel Domingues <daniel.domingues@olx.com>
2020-09-03 11:59:10 +0100

    rebase with https://github.com/sirupsen/logrus tag 1.6.0

--- json_formatter.go
+++ json_formatter.go
@@ -55,9 +55,6 @@ type JSONFormatter struct {
 
 	// PrettyPrint will indent all json logs
 	PrettyPrint bool
-
-	// EscapeHTML specifies whether problematic HTML characters
-	EscapeHTML bool
 }
 
 // Format renders a single log entry
@@ -121,9 +118,6 @@ func (f *JSONFormatter) Format(entry *Entry) ([]byte, error) {
 	if f.PrettyPrint {
 		encoder.SetIndent("", "  ")
 	}
-
-	// EscapeHTML specifies whether problematic HTML characters
-	encoder.SetEscapeHTML(f.EscapeHTML)
 
 	if err := encoder.Encode(data); err != nil {
 		return nil, fmt.Errorf("failed to marshal fields to JSON, %v", err)
